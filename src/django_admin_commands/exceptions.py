247dbacfd669eb52a398f25fa7853071597c18e8
Lcrs123 <lucascamillo333@hotmail.com>
2025-07-21 14:29:47 -0300

    Add module to group exceptions and warnings

---
+++ src/django_admin_commands/exceptions.py
@@ -0,0 +1,60 @@
+from django.core.checks import Error, Warning
+from django.core.management import get_commands
+from .consts import ADMIN_COMMANDS_SETTINGS_HINT, ADMIN_COMMANDS_SETTINGS_NAME, APP_NAME
+from django.core.exceptions import ImproperlyConfigured, PermissionDenied
+
+class AppNotFoundError(Error):
+    def __init__(self, app_name: str, id: str = f"{APP_NAME}.E001") -> None:
+        super().__init__(
+            f"App '{app_name}' is not in INSTALLED_APPS",
+            hint="The app name should be one of those in INSTALLED_APPS or 'django.core' for the django default commands",
+            id=id,
+        )
+
+
+class CommandNotFoundError(Error):
+    def __init__(
+        self, app_name: str, command_name: str, id: str = f"{APP_NAME}.E002"
+    ) -> None:
+        super().__init__(
+            f"Command '{command_name}' not found for app '{app_name}'",
+            hint=f"Avaliable commands for app '{app_name}' are {[command for command, app in get_commands().items() if app == app_name]}",
+            id=id,
+        )
+
+
+class NoCommandsFoundWarning(Warning):
+    def __init__(self, app_name: str, id: str = f"{APP_NAME}.W001") -> None:
+        super().__init__(
+            f"The config for App '{app_name}' is set to '__all__' but no commands were found for the app",
+            id=id,
+        )
+
+
+class ConfigNotSetWarning(Warning):
+    def __init__(self, id: str = f"{APP_NAME}.W002") -> None:
+        super().__init__(
+            f"Setting '{ADMIN_COMMANDS_SETTINGS_NAME}' is not set. No commands will be shown.",
+            hint=ADMIN_COMMANDS_SETTINGS_HINT,
+            id=id,
+        )
+
+class CommandsImproperlyConfigured(ImproperlyConfigured):
+    """Default ImproperlyConfigured exception"""
+
+    def __init__(
+        self,
+        setting_values: str,
+        message: str = f"Setting '{ADMIN_COMMANDS_SETTINGS_NAME}' is improperly configured. {ADMIN_COMMANDS_SETTINGS_HINT}",
+    ) -> None:
+        """ImproperlyConfigured exception with default message
+
+        Args:
+            setting_values (str): Should be the string value of the settings with the name defined in ADMIN_COMMANDS_SETTINGS_NAME
+            message (str, optional): Default message for the exception. Defaults to f"Setting '{ADMIN_COMMANDS_SETTINGS_NAME}' is improperly configured. {ADMIN_COMMANDS_SETTINGS_HINT}".
+        """
+        super().__init__(message + f"The setting current values are {setting_values}")
+
+class RunCommandPermissionError(PermissionDenied):
+    def __init__(self, msg:str="User does not have permission to run commands") -> None:
+        super().__init__(msg)
