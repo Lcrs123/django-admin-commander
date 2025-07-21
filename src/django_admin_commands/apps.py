ee312b497a1ff061aafdd648a2581aeeadc848c3
Lcrs123 <lucascamillo333@hotmail.com>
2025-07-21 14:32:39 -0300

    Change appconfig to use app name const and change verbose name

--- src/django_admin_commands/apps.py
+++ src/django_admin_commands/apps.py
@@ -1,9 +1,10 @@
 from django.apps import AppConfig
-
+from .consts import APP_NAME
 
 class AdminCommandsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "django_admin_commands"
+    name = APP_NAME
+    verbose_name = APP_NAME.replace("_"," ").title()
 
     def ready(self) -> None:
         from . import checks
