bfa99ffd1cc3e3f0de208d85671472cf03e9128a
Lcrs123 <lucascamillo333@hotmail.com>
2025-07-21 14:31:25 -0300

    Add custom permission for running command

--- src/django_admin_commands/models.py
+++ src/django_admin_commands/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from .consts import PERMISSION_NAME
 # Create your models here.
 
 
@@ -7,3 +7,7 @@ class DummyCommandModel(models.Model):
     class Meta:
         verbose_name = "Run Management Command"
         managed = False
+        default_permissions = []
+        permissions = [
+            (PERMISSION_NAME, "Can run management commands")
+        ]
