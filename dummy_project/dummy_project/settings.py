055a99227fea2ac8321533c03edbcbef4e43c240
Lcrs123 <lucascamillo333@hotmail.com>
2025-07-22 14:03:10 -0300

    Renaming app_name constant

--- dummy_project/dummy_project/settings.py
+++ dummy_project/dummy_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "django_admin_commands",
+    "django_admin_commander",
 ]
 
 MIDDLEWARE = [
